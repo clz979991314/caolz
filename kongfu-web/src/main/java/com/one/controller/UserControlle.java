9198e476778348f9d97dce33ba2d8af2baa4f93b
caolz <caolz@jheading.com>
2017-04-04 13:42:35 +0800

    cc推送

--- kongfu-web/src/main/java/com/one/controller/UserControlle.java
+++ kongfu-web/src/main/java/com/one/controller/UserControlle.java
@@ -18,7 +18,7 @@ public class UserControlle {
     private IUserService userService;
     
 	/**
-	 * 用户初始化 test2-git two 分支推送
+	 * 用户初始化 test2-git two 分支推送  ---模拟冲突
 	 * @param request
 	 * @param model
 	 * @return
